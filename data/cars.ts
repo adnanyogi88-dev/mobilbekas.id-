3e4efe8b70a556c68daa91327773348b6d179435
adnanyogi88-dev <adnanyogi88@gmail.com>
2026-08-25 11:11:08 +0700

    Improve vehicle catalog data

--- data/cars.ts
+++ data/cars.ts
@@ -1,5 +1,5 @@
 export const cars=[
- {id:1,name:'Toyota Avanza Veloz',year:2022,price:185000000,location:'Jakarta'},
- {id:2,name:'Honda Brio Satya',year:2023,price:165000000,location:'Bandung'},
- {id:3,name:'Mitsubishi Pajero Sport',year:2021,price:390000000,location:'Surabaya'}
+{id:'1',name:'Toyota Avanza Veloz',year:2022,price:'Rp185.000.000',km:'35.000 KM',location:'Jakarta',transmission:'Automatic'},
+{id:'2',name:'Honda Brio Satya',year:2023,price:'Rp165.000.000',km:'20.000 KM',location:'Bandung',transmission:'Manual'},
+{id:'3',name:'Mitsubishi Pajero Sport',year:2021,price:'Rp390.000.000',km:'45.000 KM',location:'Surabaya',transmission:'Automatic'}
 ];
