c50e929794241cae8d47ec8557a7e0e54e2b338a
adnanyogi88-dev <adnanyogi88@gmail.com>
2026-08-25 11:08:29 +0700

    Add initial car listing data

---
+++ data/cars.ts
@@ -0,0 +1,5 @@
+export const cars=[
+ {id:1,name:'Toyota Avanza Veloz',year:2022,price:185000000,location:'Jakarta'},
+ {id:2,name:'Honda Brio Satya',year:2023,price:165000000,location:'Bandung'},
+ {id:3,name:'Mitsubishi Pajero Sport',year:2021,price:390000000,location:'Surabaya'}
+];
