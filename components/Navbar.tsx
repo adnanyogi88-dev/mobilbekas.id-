ea68d9d1a93efdac25372b4de56316114a27923f
adnanyogi88-dev <adnanyogi88@gmail.com>
2026-08-25 11:08:23 +0700

    Add responsive navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+ return <header className="bg-white shadow px-8 py-4 flex justify-between items-center">
+  <div className="text-xl font-bold text-red-600">MobilBekas.id</div>
+  <nav className="flex gap-5 text-sm">
+   <span>Cari Mobil</span>
+   <span>Jual Mobil</span>
+   <span>Dealer</span>
+   <span>Masuk</span>
+  </nav>
+ </header>
+}
